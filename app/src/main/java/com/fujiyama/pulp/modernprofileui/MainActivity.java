79acbea1bf42bbd767a867e171c09860e78a0a60
Eagan Martin <mece.martinece@gmail.com>
2018-09-04 19:45:46 +0800

    Added utility to remove underline on autoLink TextViews;

--- app/src/main/java/com/fujiyama/pulp/modernprofileui/MainActivity.java
+++ app/src/main/java/com/fujiyama/pulp/modernprofileui/MainActivity.java
@@ -2,6 +2,8 @@ package com.fujiyama.pulp.modernprofileui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView email = (TextView)findViewById(R.id.txtEmail);
+        TextView phone = (TextView)findViewById(R.id.txtPhone);
+        TextView repo = (TextView)findViewById(R.id.txtRepo);
+        TextView social = (TextView)findViewById(R.id.txtSocial);
+
+        URLSpanNoUnderline.removeUnderlines((Spannable)email.getText());
+        URLSpanNoUnderline.removeUnderlines((Spannable)phone.getText());
+        URLSpanNoUnderline.removeUnderlines((Spannable)repo.getText());
+        URLSpanNoUnderline.removeUnderlines((Spannable)social.getText());
     }
 }
